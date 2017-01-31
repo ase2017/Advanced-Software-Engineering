65415e9320eac856ac246ec18e498bedc8d9610d
HectorIX <giorgoschiotis@protonmail.com>
2017-01-31 14:28:24 +0000

    Edit main file

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,9 @@
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
 		
 			System.out.println("Hello");
-=======
 
-		TaxiService taxiService = new TaxiService();
-
-		taxiService.start();
-
->>>>>>> refs/remotes/origin/master
 	}
 
 }
