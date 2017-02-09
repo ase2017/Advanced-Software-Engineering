99b43780006cdddf1ddad6b7728a9da4df87cb31
HectorIX <giorgoschiotis@protonmail.com>
2017-02-09 20:43:07 +0000

    Validator Comments v0.2

--- src/main/utils/DataFormatValidator.java
+++ src/main/utils/DataFormatValidator.java
@@ -12,8 +12,12 @@ import java.io.IOException;
  * @author Hector
  * 
  * DataFormatValidator exclusively contains static instance variables and methods so to be accessible 
- * from other classes. It validates all the input data from the four data files <taxis.txt>, <journeys.txt>, 
- * <destinations_2016.txt> and <destinations_2017.txt>.   
+ * from all the other classes. It validates all the input data from the four data files <taxis.txt>, <journeys.txt>, 
+ * <destinations_2016.txt> and <destinations_2017.txt> just before the creation of the objects.  
+ *  
+ * The instance variables are static and final, so that cannot be mutable from the methods. 
+ * 
+ * Methods are static, so to be accessible from each of the three constructors, (Taxi, Journey and Destination).
  *
  */
 public class DataFormatValidator {
@@ -38,8 +42,8 @@ public class DataFormatValidator {
 	 * Validates the taxi's registration number, as to its length.
 	 * 
 	 * @param registrationNumber
-	 * @return true if the registration number's length equals the length that indicates
-	 *         the corresponding instance variable. 
+	 * @return true if the registration number's length equals the indicated length 
+	 *         by the corresponding instance variable. 
 	 *         Else return false. 
 	 */
 	public static boolean validateRegistrationNumber(String registrationNumber) {
@@ -51,6 +55,14 @@ public class DataFormatValidator {
 
 	}
 
+	/**
+	 * Validates the driver's name, as to the range of its length. 
+	 * 
+	 * @param driverName
+	 * @return true if the taxi's driver name is in the indicated range 
+	 *         by the corresponding instance variables.
+	 *         Else return false. 
+	 */
 	public static boolean validateDriverName(String driverName) {
 
 		if (driverName.length() >= Integer.parseInt(MIN_DRIVER_NAME_LENGTH)
@@ -60,6 +72,14 @@ public class DataFormatValidator {
 			return false;
 	}
 
+	/**
+	 * Validates the taxi's brand name, as to the range of its length. 
+	 * 
+	 * @param brandName
+	 * @return true if the brand name is in the indicated range by the
+	 *         corresponding instance variables.
+	 *         Else return false. 
+	 */
 	public static boolean validateBrand(String brandName) {
 
 		if (brandName.length() >= Integer.parseInt(MIN_BRAND_NAME)
@@ -69,6 +89,14 @@ public class DataFormatValidator {
 			return false;
 	}
 
+	/**
+	 * Validates the journey's destination name, as to the range of its length. 
+	 * 
+	 * @param destinationName
+	 * @return true if the destination's name is in the indicated range 
+	 *              by the corresponding instance variables.
+	 *              Else return false. 
+	 */
 	public static boolean validateDestinationName(String destinationName) {
 
 		if (destinationName.length() >= Integer.parseInt(MIN_DESTINATION_NAME_LENGTH)
@@ -78,6 +106,14 @@ public class DataFormatValidator {
 			return false;
 	}
 
+	/**
+	 * Validates the distance covered by the taxi in a journey, as to a specified range. 
+	 * 
+	 * @param distance
+	 * @return true if the distance covered by the taxi is in the indicated range
+	 *              by the corresponding instance variables. 
+	 *              Else return false. 
+	 */
 	public static boolean validateDistance(double distance) {
 
 		if (distance > MIN_DISTANCE && distance <= MAX_DISTANCE)
@@ -86,6 +122,13 @@ public class DataFormatValidator {
 			return false;
 	}
 
+	/**
+	 * Validates the number of the passenger, as to a specified range. 
+	 * 
+	 * @param passengerNumber
+	 * @return true if the number of the passengers are in the indicated range by the
+	 *         corresponding instance variables. 
+	 */
 	public static boolean validatePassengerNumber(int passengerNumber) {
 
 		if (passengerNumber >= MIN_NUMBER_OF_PASSENGERS && passengerNumber <= MAX_NUMBER_OF_PASSENGERS)
@@ -94,6 +137,13 @@ public class DataFormatValidator {
 			return false;
 	}
 
+	/**
+	 * Validates the time needed a journey, as to its sign.
+	 * 
+	 * @param time
+	 * @return true if time is positive.
+	 *         Else return false. 
+	 */
 	public static boolean validateTime(double time) {
 
 		if (time > MIN_TIME)
@@ -102,6 +152,13 @@ public class DataFormatValidator {
 			return false;
 	}
 
+	/**
+	 * Validates the taxi's maximum velocity of a journey. 
+	 * 
+	 * @param maxVelocity
+	 * @return true if the maximum velocity is in the indicated range
+	 *         by the corresponding instance variables.
+	 */
 	public static boolean validateMaximumVelocity(double maxVelocity) {
 
 		if (maxVelocity >= MIN_VELOCITY && maxVelocity <= MAX_VELOCITY)
@@ -110,6 +167,12 @@ public class DataFormatValidator {
 			return false;
 	}
 
+	/**
+	 * Validates whether the destination is in urban area or not.
+	 * 
+	 * @param urban
+	 * @return
+	 */
 	public static boolean validateUrban(String urban) {
 
 		if (urban == "Y" || urban == "N")
