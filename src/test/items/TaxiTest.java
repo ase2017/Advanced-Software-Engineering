e7f002ebdc06587bd628204a147e4b5f57199082
ase2017 <goniotis@hotmail.gr>
2017-02-17 11:38:57 +0000

    JUnit Tests + updated validateRegistrationNumber method.

--- src/test/items/TaxiTest.java
+++ src/test/items/TaxiTest.java
@@ -1,4 +1,97 @@
 package test.items;
 
+import main.items.*;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 public class TaxiTest {
+	
+	private String trueRegistrationNumber = "AA123AB";
+	private String trueDriverName = "John Bend";
+	private String trueBrand = "Toyota";
+	
+	@Test
+	public void testWrongLengthRegistrationNumber(){
+		
+		final ByteArrayOutputStream serialContent = new ByteArrayOutputStream();
+		String wrongRegistrationNumber = "A1A";
+		
+		System.setOut(new PrintStream(serialContent));
+		new Taxi(wrongRegistrationNumber, trueDriverName, trueBrand);
+		
+		assertEquals("Error! Wrong registration number (in file taxis.txt in line: 0 ).\r\n", serialContent.toString());
+	}
+	
+	@Test
+	public void testWrongPatternRegistrationNumber(){
+		
+		final ByteArrayOutputStream serialContent = new ByteArrayOutputStream();
+		String wrongRegistrationNumber = "A1A";
+		
+		System.setOut(new PrintStream(serialContent));
+		new Taxi(wrongRegistrationNumber, trueDriverName, trueBrand);
+		
+		assertEquals("Error! Wrong registration number (in file taxis.txt in line: 0 ).\r\n", serialContent.toString());
+		
+	}
+	
+	@Test
+	public void testWrongLengthDriverName(){
+		
+		final ByteArrayOutputStream serialContent = new ByteArrayOutputStream();
+		String wrongDriverName = "Ja"; //Just checks if driver`s name is between 3 and 30 characters, other check will be implemented in DataFileReaderTest
+		
+		System.setOut(new PrintStream(serialContent));
+		new Taxi(trueRegistrationNumber, wrongDriverName, trueBrand);
+		
+		assertEquals("Error! Wrong driver`s name (in file taxis.txt in line: 0 ).\r\n", serialContent.toString());
+	}
+
+	
+	@Test
+	public void testNullDriverName(){
+		
+		final ByteArrayOutputStream serialContent = new ByteArrayOutputStream();
+		String wrongDriverName = null; //Just checks if driver`s name is between 3 and 30 characters, other check will be implemented in DataFileReaderTest
+		
+		System.setOut(new PrintStream(serialContent));
+		new Taxi(trueRegistrationNumber, wrongDriverName, trueBrand);
+		
+		assertEquals("Error! Wrong driver`s name (in file taxis.txt in line: 0 ).\r\n", serialContent.toString());
+	}
+
+	@Test
+	public void testNullBrandName(){
+		
+		final ByteArrayOutputStream serialContent = new ByteArrayOutputStream();
+		String wrongBrandName = null;
+		
+		System.setOut((new PrintStream(serialContent)));
+		new Taxi(trueRegistrationNumber, trueDriverName, wrongBrandName);
+		
+		assertEquals("Error! Wrong car`s brand (in file taxis.txt in line: 0 ).\r\n", serialContent.toString());
+	}
+	
+	@Test
+	public void testWrongLengthBrandName(){
+		
+		final ByteArrayOutputStream serialContent = new ByteArrayOutputStream();
+		String wrongBrandName = "J";
+		
+		System.setOut(new PrintStream(serialContent));
+		new Taxi(trueRegistrationNumber, trueDriverName, wrongBrandName);
+		
+		assertEquals("Error! Wrong car`s brand (in file taxis.txt in line: 0 ).\r\n", serialContent.toString());
+	}
+	
+	@Test
+	public void testCorrectExample(){
+		
+		Taxi ex = new Taxi(trueRegistrationNumber, trueDriverName, trueBrand);
+		boolean result = ex.equals(null);
+		
+		assertFalse(result);
+	}
 }
--- src/test/view/MenuDisplayerTest.java
+++ src/test/view/MenuDisplayerTest.java
@@ -1,4 +1,27 @@
 package test.view;
 
+import main.view.*;
+import static org.junit.Assert.*;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.Test;
+
 public class MenuDisplayerTest {
+	
+	@Test
+	public void showMenuTest(){
+		
+		String expectedMenuContent = "%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n\r\n\t 1."
+				+ " Create output files.\r\n\t 2. Exit.\n\r\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"
+				+ "%%%%%%%%%%%\n\r\n";
+		
+		final ByteArrayOutputStream serialContent = new ByteArrayOutputStream();
+		
+		System.setOut(new PrintStream(serialContent));
+		MenuDisplayer md = new MenuDisplayer();
+		md.displayMainMenu();
+		
+		assertEquals(expectedMenuContent, serialContent.toString());
+	}
 }
