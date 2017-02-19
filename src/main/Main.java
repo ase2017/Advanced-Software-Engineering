1e840d10429066483fd959645739fa41228e4fc8
ase2017 <goniotis@hotmail.gr>
2017-02-19 17:02:30 +0000

    Test change

--- src/main/Main.java
+++ src/main/Main.java
@@ -22,8 +22,8 @@ public class Main {
 
 	 public static void main(String[] args) {
  		
-		 TaxiService taxiService = new TaxiService();
-		 taxiService.start();
+		 TaxiService tx = new TaxiService();
+		 tx.start();
 
  	}
 }
