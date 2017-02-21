692052847b2fdcedda0022b93b1785600ebcda40
julios <detriejule@eisti.eu>
2017-02-21 18:43:12 +0000

    fixed some bugs

--- src/main/io/DataFileWriter.java
+++ src/main/io/DataFileWriter.java
@@ -17,10 +17,10 @@ import main.items.TaxiData;
  */
 public class DataFileWriter {
 
-	private static final String OUTPUT_DIRECTORY_NAME = "outputFiles";
-	private static final String FILE_NAME_TOP_5 = "top-5.txt";
-	private static final String FILE_NAME_PLACES_PER_DRIVER = "driver-Journeys.txt";
-	private static final String FILE_NAME_PLACES = "visited-Places.txt";
+	public static final String OUTPUT_DIRECTORY_NAME = "outputFiles";
+	public static final String FILE_NAME_TOP_5 = "top-5.txt";
+	public static final String FILE_NAME_PLACES_PER_DRIVER = "driver-Journeys.txt";
+	public static final String FILE_NAME_PLACES = "visited-Places.txt";
 
 	/**
 	 * writeFiles is a public method that takes the appropriate Stings
@@ -31,49 +31,57 @@ public class DataFileWriter {
 	 */
 	public void writeFiles(TaxiData taxidata) {
 
-		boolean file1_OK = false;
-		boolean file2_OK = false;
-		boolean file3_OK = false;
 
-		System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
-		System.out.println("\nCreating output files...\n");
-
-		// Creates the output Directory, if does not exists.
-		File f = new File(OUTPUT_DIRECTORY_NAME);
-		if (!f.isDirectory()) {
-			f.mkdir();
-		}
 
 		// Call the three methods in order to create the three output files.
-		file1_OK = writeFile1(taxidata);
-		file2_OK = writeFile2(taxidata);
-		file3_OK = writeFile3(taxidata);
+		if(taxidata != null) {
+
+			boolean file1_OK = false;
+			boolean file2_OK = false;
+			boolean file3_OK = false;
+
+			System.out.println("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%");
+			System.out.println("\nCreating output files...\n");
+
+			// Creates the output Directory, if does not exists.
+			File f = new File(OUTPUT_DIRECTORY_NAME);
+			if (!f.isDirectory()) {
+				f.mkdir();
+			}
+			file1_OK = writeFile1(taxidata);
+			file2_OK = writeFile2(taxidata);
+			file3_OK = writeFile3(taxidata);
+
+			// Inform user whether the process was successful or not.
+			if ( file1_OK )
+				System.out.println("\t+ <" + FILE_NAME_TOP_5 + "> successfully created!");
+			else
+				System.out.println("\t->Error occured in " + FILE_NAME_TOP_5 + "...");
+
+			if ( file2_OK )
+				System.out.println("\t+ <" + FILE_NAME_PLACES_PER_DRIVER + "> successfully created!");
+			else
+				System.out.println("\t->Error occured in " + FILE_NAME_PLACES_PER_DRIVER + "...");
+
+			if ( file3_OK )
+				System.out.println("\t+ <" + FILE_NAME_PLACES + "> successfully created!");
+			else
+				System.out.println("\t->Error occured in " + FILE_NAME_PLACES + "...");
 
 
-		// Inform user whether the process was successful or not.
-		if ( file1_OK )
-			System.out.println("\t+ <" + FILE_NAME_TOP_5 + "> successfully created!");
-		else
-			System.out.println("\t->Error occured in " + FILE_NAME_TOP_5 + "...");
-
-		if ( file2_OK )
-			System.out.println("\t+ <" + FILE_NAME_PLACES_PER_DRIVER + "> successfully created!");
-		else
-			System.out.println("\t->Error occured in " + FILE_NAME_PLACES_PER_DRIVER + "...");
-
-		if ( file3_OK )
-			System.out.println("\t+ <" + FILE_NAME_PLACES + "> successfully created!");
-		else
-			System.out.println("\t->Error occured in " + FILE_NAME_PLACES + "...");
+			if ( file1_OK && file2_OK && file3_OK )
+				System.out.println("\nProcess completed!!!");
+			else
+				System.out.println("\nProcess ended with errors...");
 
 
-		if ( file1_OK && file2_OK && file3_OK )
-			System.out.println("\nProcess completed!!!");
-		else
-			System.out.println("\nProcess ended with errors...");
+			System.out.println("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+		} else {
+			throw new NullPointerException("TaxiData is null!");
+		}
 
 
-		System.out.println("\n%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n");
+
 
 	}
 
@@ -87,26 +95,30 @@ public class DataFileWriter {
 	 * @return true if process ended successfully
 	 * 	       else return false.
 	 */
-	public boolean writeFile1(TaxiData taxidata) {
-
-
-		BufferedWriter buffWriterFile1 = null;
-		FileWriter fileWriterFile1 = null;
+	public boolean writeFile1(TaxiData taxidata){
 
 		if(taxidata == null) {
 			throw new NullPointerException("TaxiData is null!");
 		} else {
+
+			BufferedWriter buffWriterFile1 = null;
+			FileWriter fileWriterFile1 = null;
+
 			try {
 
 				String fileContents = "";
 
 				// Open the FILE_NAME_TOP_5 (instance variable) file.
 				fileWriterFile1 = new FileWriter( OUTPUT_DIRECTORY_NAME + "/" + FILE_NAME_TOP_5 );
-				buffWriterFile1 = new BufferedWriter(fileWriterFile1);
+
+				if(fileWriterFile1 != null)
+					buffWriterFile1 = new BufferedWriter(fileWriterFile1);
 
 
 				fileContents = taxidata.formatJourneyFile();	// Takes the resulted data from the Taxi's Data object.
-				buffWriterFile1.write(fileContents);			// Writes the data to the file
+
+				if(buffWriterFile1 != null && fileContents != null)
+					buffWriterFile1.write(fileContents);			// Writes the data to the file
 
 
 			} catch (IOException | NullPointerException e) {
@@ -154,6 +166,7 @@ public class DataFileWriter {
 		if(taxidata == null) {
 			throw new NullPointerException("TaxiData is null!");
 		} else {
+
 			BufferedWriter buffWriterFile2 = null;
 			FileWriter fileWriterFile2 = null;
 
@@ -163,10 +176,15 @@ public class DataFileWriter {
 
 				// Open the FILE_NAME_PLACES_PER_DRIVER (instance variable) file.
 				fileWriterFile2 = new FileWriter(OUTPUT_DIRECTORY_NAME + "/" + FILE_NAME_PLACES_PER_DRIVER);
-				buffWriterFile2 = new BufferedWriter(fileWriterFile2);
+
+				if(fileWriterFile2 != null)
+					buffWriterFile2 = new BufferedWriter(fileWriterFile2);
+
 
 				fileContents = taxidata.formatPlacesVisitedPerDriver();        // Takes the resulted data from the Taxi's Data object.
-				buffWriterFile2.write(fileContents);                        // Writes the data to the file.
+
+				if(buffWriterFile2 != null && fileContents != null)
+					buffWriterFile2.write(fileContents);                        // Writes the data to the file.
 
 
 			} catch (IOException | NullPointerException e) {
@@ -213,6 +231,7 @@ public class DataFileWriter {
 		if(taxidata == null) {
 			throw new NullPointerException("TaxiData is null!");
 		} else {
+
 			BufferedWriter buffWriterFile3 = null;
 			FileWriter fileWriterFile3 = null;
 
@@ -221,10 +240,15 @@ public class DataFileWriter {
 				String fileContents = "";
 
 				fileWriterFile3 = new FileWriter(OUTPUT_DIRECTORY_NAME + "/" + FILE_NAME_PLACES);
-				buffWriterFile3 = new BufferedWriter(fileWriterFile3);
+
+				if(fileWriterFile3 != null)
+					buffWriterFile3 = new BufferedWriter(fileWriterFile3);
+
 
 				fileContents = taxidata.formatPlacesVisited();        // Takes the resulted data from the Taxi's Data object.
-				buffWriterFile3.write(fileContents);                // Writes the data to the file.
+
+				if(buffWriterFile3 != null && fileContents != null)
+					buffWriterFile3.write(fileContents);                // Writes the data to the file.
 
 
 			} catch (IOException | NullPointerException e) {
